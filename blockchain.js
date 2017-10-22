62d6a9cb7a8b9cd30c68e0ad0bc8c3a6fc6ee256
31431 <kitsada280@gmail.com>
2017-10-21 17:53:57 -0700

    change index.js to blockchain.js && server.js to index.js

---
+++ blockchain.js
@@ -0,0 +1,117 @@
+// require('./server.js');
+
+var blockchain = require('mastercard-blockchain');
+var MasterCardAPI = blockchain.MasterCardAPI;
+var config = require('./config.json');
+var fs = require('fs');
+
+
+var consumerKey = config.CONSUMER_KEY;   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
+var keyStorePath = config.KEYSTORE_PATH; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
+var keyAlias = config.KEY_ALIAS;   // For production: change this to the key alias you chose when you created your production key
+var keyPassword = config.KEY_PASSWORD;   // For production: change this to the key alias you chose when you created your production key
+var appID = "TM25";
+
+// users 
+var users = JSON.parse(fs.readFileSync('./JSON/Users.json', 'utf8'))
+var NodeRSA = require('node-rsa');
+users.forEach(function(user) {
+	// generate RSA keypair 
+	var key = new NodeRSA({b: 256});
+	user.rsakey = key; // stored just for testing
+});
+
+
+
+// protobuf 
+var protobuf = require('protobufjs');
+var protoFile = 'message.proto';
+var encoding = 'base64';
+
+function initAPI() {
+	console.log("initializing");
+	var authentication = new MasterCardAPI.OAuth(consumerKey, keyStorePath, keyAlias, keyPassword);
+	MasterCardAPI.init({
+		sandbox: true,
+		debug: true,
+		authentication: authentication
+	});
+	loadProtobuf();
+	//console.log('---------------Loading Data....');
+	// loadData();
+}
+
+var msgClass; 
+function loadProtobuf() {
+	protobuf.load(protoFile, function(err, root) {
+		console.log('loading protobuf');
+		if (err) {
+			console.log('error', err);
+		}
+		msgClass = root.lookupType("TM25.Transaction");
+		console.log(msgClass);
+		blockchain.App.update({
+			id: appID,
+			name: appID,
+			description: "",
+			version: 0,
+			definition: {
+				format: "proto3",
+				encoding: encoding,
+				messages: fs.readFileSync(protoFile).toString(encoding)
+			}
+		}, function(error, data) {
+			console.log('updated app');
+			if (error) {
+				console.error("HttpStatus: "+error.getHttpStatus());
+				console.error("Message: "+error.getMessage());
+				console.error("ReasonCode: "+error.getReasonCode());
+				console.error("Source: "+error.getSource());
+				console.error(error)
+			}
+			
+		});
+	});
+}
+
+initAPI(); 
+
+function createEntry() {
+	console.log("create entry"); 
+	var payload = {
+		reference: "EXAMPLE MESSAGE", 
+		owner_pk: users[0].rsakey.exportKey('public'), 
+		recipient_pk: users[1].rsakey.exportKey('public'), 
+		type: 0
+	}; 
+	var err = msgClass.verify(payload);
+	if (err) {
+		console.log(msgClass, err);
+	} else {
+		var message = msgClass.create(payload);
+		blockchain.TransactionEntry.create({
+			"app": appID,
+			"encoding": encoding,
+			"value": msgClassDef.encode(message).finish().toString(encoding)
+		}, function(err, result) {
+			if (err) {
+				console.log('error', err);
+			} else {
+				console.log(result);
+			}
+		});
+	}
+	
+}
+
+function getProperties(obj) {
+    var ret = [];
+    for (var name in obj) {
+        if (obj.hasOwnProperty(name)) {
+            ret.push(name);
+        }
+    }
+    return ret;
+}
+
+createEntry();
--- index.js
+++ index.js
@@ -1,117 +1,55 @@
-// require('./server.js');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+//const {User} = require('./db/model/users.js');
 
-var blockchain = require('mastercard-blockchain');
-var MasterCardAPI = blockchain.MasterCardAPI;
-var config = require('./config.json');
-var fs = require('fs');
+const app = express();
+const port = process.env.port || 3000;
+
+const userData = require('./JSON/Users.json');
+const rewardsCat = require('./JSON/RewardsCat.json');
+const owners = require('./JSON/Owners.json');
+
+console.log(userData);
+console.log(rewardsCat);
+console.log(owners);
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//Home Page
+app.get('/',(req,res)=>{
+	console.log('hi');
+	res.sendFile(path.join(__dirname+'/test.html'));
+})
+
+app.post('/login',(req,res)=>{
+	console.log(req.body.username);
+	console.log(req.body.password);
+	console.log('Logging in');
+	res.send()
+})
+
+//Search
+app.get('/search',(req,res)=>{
+	console.log('Loading a search page.');
+	console.log(userData);
+})
+
+app.post('/postEntry',(req,res)=>{
+	var owner_id = req.body.owner_id;
+	var recipient_id = req.body.recipient_id;
+	var object_id = req.body.object_id;
+
+	createEntry(owner_id, recipient_id, object_id);x
+
+})
 
 
-var consumerKey = config.CONSUMER_KEY;   // You should copy this from "My Keys" on your project page e.g. UTfbhDCSeNYvJpLL5l028sWL9it739PYh6LU5lZja15xcRpY!fd209e6c579dc9d7be52da93d35ae6b6c167c174690b72fa
-var keyStorePath = config.KEYSTORE_PATH; // e.g. /Users/yourname/project/sandbox.p12 | C:\Users\yourname\project\sandbox.p12
-var keyAlias = config.KEY_ALIAS;   // For production: change this to the key alias you chose when you created your production key
-var keyPassword = config.KEY_PASSWORD;   // For production: change this to the key alias you chose when you created your production key
-var appID = "TM25";
 
-// users 
-var users = JSON.parse(fs.readFileSync('./JSON/Users.json', 'utf8'))
-var NodeRSA = require('node-rsa');
-users.forEach(function(user) {
-	// generate RSA keypair 
-	var key = new NodeRSA({b: 256});
-	user.rsakey = key; // stored just for testing
+app.listen(port,()=>{
+	console.log(`Node server started on Port ${port} at `, Date());
 });
 
-
-
-// protobuf 
-var protobuf = require('protobufjs');
-var protoFile = 'message.proto';
-var encoding = 'base64';
-
-function initAPI() {
-	console.log("initializing");
-	var authentication = new MasterCardAPI.OAuth(consumerKey, keyStorePath, keyAlias, keyPassword);
-	MasterCardAPI.init({
-		sandbox: true,
-		debug: true,
-		authentication: authentication
-	});
-	loadProtobuf();
-	//console.log('---------------Loading Data....');
-	// loadData();
+function retrieveSearchResults (){ 
 }
-
-var msgClass; 
-function loadProtobuf() {
-	protobuf.load(protoFile, function(err, root) {
-		console.log('loading protobuf');
-		if (err) {
-			console.log('error', err);
-		}
-		msgClass = root.lookupType("TM25.Transaction");
-		console.log(msgClass);
-		blockchain.App.update({
-			id: appID,
-			name: appID,
-			description: "",
-			version: 0,
-			definition: {
-				format: "proto3",
-				encoding: encoding,
-				messages: fs.readFileSync(protoFile).toString(encoding)
-			}
-		}, function(error, data) {
-			console.log('updated app');
-			if (error) {
-				console.error("HttpStatus: "+error.getHttpStatus());
-				console.error("Message: "+error.getMessage());
-				console.error("ReasonCode: "+error.getReasonCode());
-				console.error("Source: "+error.getSource());
-				console.error(error)
-			}
-			
-		});
-	});
-}
-
-initAPI(); 
-
-function createEntry() {
-	console.log("create entry"); 
-	var payload = {
-		reference: "EXAMPLE MESSAGE", 
-		owner_pk: users[0].rsakey.exportKey('public'), 
-		recipient_pk: users[1].rsakey.exportKey('public'), 
-		type: 0
-	}; 
-	var err = msgClass.verify(payload);
-	if (err) {
-		console.log(msgClass, err);
-	} else {
-		var message = msgClass.create(payload);
-		blockchain.TransactionEntry.create({
-			"app": appID,
-			"encoding": encoding,
-			"value": msgClassDef.encode(message).finish().toString(encoding)
-		}, function(err, result) {
-			if (err) {
-				console.log('error', err);
-			} else {
-				console.log(result);
-			}
-		});
-	}
-	
-}
-
-function getProperties(obj) {
-    var ret = [];
-    for (var name in obj) {
-        if (obj.hasOwnProperty(name)) {
-            ret.push(name);
-        }
-    }
-    return ret;
-}
-
-createEntry();
